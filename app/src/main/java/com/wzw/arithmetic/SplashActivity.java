fad8599e75352e97ac41909308b2132db844af74
wenziwen <wenzw@zdnst.com>
2017-09-08 17:52:32 +0800

    添加设置页

---
+++ app/src/main/java/com/wzw/arithmetic/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.wzw.arithmetic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 计算范围
+        // 次数
+        // 发音
+        // 四则运算
+
+    }
+}
